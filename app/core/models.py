c943d06b8c1d792e0fb7ef45de475ee6407fe9bf
Sarah Hall <swatson1994@yahoo.com>
2023-04-04 19:30:45 -0400

    Assign UserManager to user and update settings with Auth for user model

--- app/core/models.py
+++ app/core/models.py
@@ -19,12 +19,14 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
+
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
     email = models.EmailField(maxlength=255, unique=True)
     name = models.Charfield(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
